10563adc833798db6b4e8c910acc80a3fb00197a
RXstoinkboy <erykludwin@gmail.com>
2019-02-03 00:48:32 +0100

    Prepared component. Started setting up react ruoter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom'; 
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Defaults from './components/Defaults';
+import Details from './components/Details';
+import Cart from './components/Cart';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">column number one</div>
-          <div className="col-6">column number one<i class="fas fa-home"></i></div>
-        </div>
-      </div>
-
+      <React.Fragment>
+        <Navbar /> {/* shouldn't be included in switch - we want to display it all the time */}
+        <Switch>
+          <Route exact {/* use "exact" for the home page. Otherwis you will always be redirected to home page unless this path is included in the end */} path="/" component={ProductList} /> { /* home path - display ProductList as default page */}
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Defaults} /> {/* display when page can't be found -> wrong path was inserted */}
+        </Switch>
+      </React.Fragment>
     );
   }
 }
