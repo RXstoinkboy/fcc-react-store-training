b1e696628f1b1b9a2b3645c6ab1ddfa91eb3434e
RXstoinkboy <erykludwin@gmail.com>
2019-02-05 23:17:04 +0100

    quick save of cart progress

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import Title from '../Title';
 import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
 import { Consumer } from '../../Context';
+import CartList from './CartList';
 
 class Cart extends Component {
     render() {
@@ -16,6 +17,8 @@ class Cart extends Component {
                                 <React.Fragment>
                                     <Title name='your' title='cart' />
                                     <CartColumns />                                    
+                                    <CartList value={context} /> 
+                                    {/* passing context as a prop to CartList */}
                                 </React.Fragment>
                             )
                         } else {
---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CartItem from './CartItem'
+
+const CartList = ({value}) => {
+    const {cart} = value; // destructuring of single item to simplify writting
+    console.log(value, cart);
+    return (
+        <div className='container-fluid'>
+            {cart.map(item => {
+                return <CartItem key={item.id} item={item} value={value} />             
+            })}
+        </div>
+    );
+};
+
+export default CartList;
