3e149825a075271304807486fb32e8e0d0ecd92b
RXstoinkboy <erykludwin@gmail.com>
2019-02-06 21:29:14 +0100

    done with +/-

--- src/Context.js
+++ src/Context.js
@@ -29,11 +29,39 @@ class Context extends Component {
 
     // cart methods
     increment =id=>{
-        console.log(`this is incremenent`)
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        
+        product.count = product.count + 1;
+        product.total = product.price * product.count;
+        
+        this.setState(()=> {
+            return ({
+                cart: [...tempCart]
+            })
+        }, ()=> {this.addTotals()})
     }
     
     decrement =id=>{
-        console.log(`this is decremenent`)
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+    
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        
+        product.count = product.count -1;
+
+        if(product.count === 0){
+            this.removeItem(id);
+        } else {
+            product.total = product.count * product.price;
+            this.setState(()=> {
+                return {cart: [...tempCart]}
+            }, ()=> {this.addTotals()})
+        }
     }
 
     removeItem =id=>{
